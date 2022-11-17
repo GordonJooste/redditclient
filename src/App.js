1b2d56eedb557ebe7ed5c2ecd8f10b641cdad1e5
Gordon Jooste <jooste44@gmail.com>
2022-11-17 21:23:34 +0700

    forgot to commit after taking the data from reddit, need to figure out how to process it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
-import React, {useState} from "react";
+//import './App.css';
+import React, {useState, useEffect} from "react";
 import { Switch, Route, Redirect, NavLink } from "react-router-dom";
-import {Post} from './components/post/post';
+//import {Post} from './components/post/post';
+import {data} from './Data';
 
 function App(){
 
-  
+  const jsonData = data;
 
   const [searchQuery, setsearchQuery] = useState([]);
   const [PostLink, setPostLink] = useState([]);
@@ -13,21 +14,25 @@ function App(){
   const [PostPicture, setPostPicture] = useState([]);
   const [CommentsList, setCommentsList] = useState([]);
 
-  const ROUTES = {
-    POSTS: "/posts",
-  };
-
-  setsearchQuery((prev)=>{ return [searchQuery, ...prev]});
-
+  
+  useEffect(() => {
+    setsearchQuery((prev) => { 
+      return [searchQuery, ...prev]
+    });
+  }, []);
+  
   return (
-    <div className='App'>
+    <div className='App' jsonData = {jsonData.data}>
       <nav>
-        <NavLink to={ROUTES.POSTS} activeClassName="active">
-          Home
-        </NavLink>
-        <Search search = {searchQuery}/>
+        
+        <div className='Search' searchquery = {searchQuery} />
       </nav>
       <main>
+        <ul>
+          <li><a>1</a></li>
+          <li><a>2</a></li>
+          <li><a>3</a></li>
+        </ul>
         //TODO Posts list
       </main>
     </div>
--- src/components/post/post.js
+++ src/components/post/post.js
@@ -1,4 +1,4 @@
-import './App.css';
+//import './App.css';
 import React, {useState} from "react";
 import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 
