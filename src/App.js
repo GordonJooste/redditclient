40294d7f6dbefe000f319bd389b6a7413f154df7
Gordon Jooste <jooste44@gmail.com>
2022-11-19 20:11:29 +0700

    figured out the JSON from the reddit API, I now am able to get the information I need for the website out of the JSON, next step is getting the promise working correctly

--- src/App.js
+++ src/App.js
@@ -1,39 +1,53 @@
 //import './App.css';
 import React, {useState, useEffect} from "react";
 import { Switch, Route, Redirect, NavLink } from "react-router-dom";
-//import {Post} from './components/post/post';
-import {data} from './Data';
+import {Post} from './components/post/post';
+import {apiCall} from './Data';
 
 function App(){
 
-  const jsonData = data;
-
   const [searchQuery, setsearchQuery] = useState([]);
-  const [PostLink, setPostLink] = useState([]);
-  const [PostText, setPostText] = useState([]);
-  const [PostPicture, setPostPicture] = useState([]);
+  const [PostsArray, setPostsArray] = useState( { }); 
   const [CommentsList, setCommentsList] = useState([]);
-
+  let result = 'lol';
   
   useEffect(() => {
     setsearchQuery((prev) => { 
       return [searchQuery, ...prev]
     });
-  }, []);
+    setPostsArray((prev) => {
+      // its not working because I need to use a promise. Get the result from the promise and then set state etc. 
+      
+      apiCall.fetchPosts('/r/popular').then((data) => {
+        console.log(`useEffect`);
+        result = data[4];    
+        console.log(result);
+        return result;
+      });
+    })
+  }, [0]);
+  
   
   return (
-    <div className='App' jsonData = {jsonData.data}>
+    <div className='App'>
       <nav>
-        
-        <div className='Search' searchquery = {searchQuery} />
+        {/*}// TODO Search <div className='Search' searchquery = {searchQuery} />*/}
       </nav>
       <main>
         <ul>
           <li><a>1</a></li>
           <li><a>2</a></li>
           <li><a>3</a></li>
+          <li><a>{ //PostsArray[4].data.subreddit
+          }</a></li>
+          <li><a>4</a></li>
+          
         </ul>
         //TODO Posts list
+        <div>
+          
+        </div>
+        
       </main>
     </div>
   );
--- src/components/post/post.js
+++ src/components/post/post.js
@@ -2,21 +2,16 @@
 import React, {useState} from "react";
 import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 
-export const Post = ({
-    PostLink,
-    PostText,
-    PostPicture,
-    CommentsList,
-})=>{
+export const Post = (props)=>{
 
     return (
         <div>
+            Hello
             //Picture at the top
             //Link to article is the picture
             //Text below
 
             //Below that,. line break and then comments
-
         </div>
     )
 
